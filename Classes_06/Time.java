9673125115545fb1d99a970e8ac81af811c8088a
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 00:04:18 +0300

    Create

---
+++ Classes_06/Time.java
@@ -0,0 +1,72 @@
+import java.util.Objects;
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+
+        if (this.hours > 60) {
+            this.hours = 0;
+        }
+
+        if (this.minutes > 60) {
+            this.minutes = 0;
+        }
+
+        if (this.seconds > 60) {
+            this.seconds = 0;
+        }
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hours, minutes, seconds);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Time other = (Time) obj;
+        return hours == other.hours && minutes == other.minutes && seconds == other.seconds;
+    }
+
+    @Override
+    public String toString() {
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+}
