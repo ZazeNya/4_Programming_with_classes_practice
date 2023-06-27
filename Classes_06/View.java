5c1ce297c06bc66f6b36819b793389d9532a5390
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 00:07:33 +0300

    Create

---
+++ Classes_06/View.java
@@ -0,0 +1,9 @@
+public class View {
+    public void printCurrentTime(String time) {
+        System.out.println("Введенное время " + time);
+    }
+
+    public void printChangedTime(String time) {
+        System.out.println("Измененное время " + time);
+    }
+}
