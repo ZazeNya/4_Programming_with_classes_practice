08a9377697aec7c6948c40afecdd5184b401e61f
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 14:19:19 +0300

    Create City.java

---
+++ Aggregation and composition/Problem_3/City.java
@@ -0,0 +1,64 @@
+package study;
+
+public class City {
+    private String name;
+    private double areaCity;
+    private int populationCity;
+    private boolean isRegionalCenter;
+    private boolean isCapital;
+
+    public City(String name, double areaCity, int populationCity, boolean isRegionalCenter, boolean isCapital) {
+        this.name = name;
+        this.areaCity = areaCity;
+        this.populationCity = populationCity;
+        this.isRegionalCenter = isRegionalCenter;
+        this.isCapital = isCapital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAreaCity() {
+        return areaCity;
+    }
+
+    public void setAreaCity(double areaCity) {
+        this.areaCity = areaCity;
+    }
+
+    public int getPopulationCity() {
+        return populationCity;
+    }
+
+    public void setPopulationCity(int populationCity) {
+        this.populationCity = populationCity;
+    }
+
+    public boolean getIsRegionalCenter() {
+        return isRegionalCenter;
+    }
+
+    public void setRegionalCenter(boolean regionalCenter) {
+        isRegionalCenter = regionalCenter;
+    }
+
+    public boolean getIsCapital() {
+        return isCapital;
+    }
+
+    public void setCapital(boolean capital) {
+        isCapital = capital;
+    }
+
+    @Override
+    public String toString() {
+        return '\'' + name + '\'' +
+                ": площадь - " + areaCity + " кв. км." +
+                ", население - " + populationCity + " чел.";
+    }
+}
