891f2f5d078f5ee5449b4132695709be4e6610fd
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 15:13:57 +0300

    Create

---
+++ Aggregation and composition/Problem_5/DataBase.java
@@ -0,0 +1,27 @@
+package study;
+import java.util.Arrays;
+public class DataBase {
+    private static Tourist tp_00 = new Tourist("Китай", "Пекин", "экскурсия", "самолет", "без питания", 3);
+    private static Tourist tp_01 = new Tourist("Румыния", "Брашов", "экскурсия", "самолет", "завтрак", 5);
+    private static Tourist tp_02 = new Tourist("Венгрия", "Будапешт", "шопинг", "самолет", "без питания", 3);
+    private static Tourist tp_03 = new Tourist("Франция", "Париж", "шопинг", "самолет", "без питания", 2);
+    private static Tourist tp_04 = new Tourist("Великобритания", "Лондон", "отдых", "самолет", "все включено", 11);
+    private static Tourist tp_05 = new Tourist("Беларусь", "Нарочь", "лечение", "автобус", "трехразовое", 14);
+    private static Tourist tp_06 = new Tourist("Беларусь", "Гродненская обл.", "экскурсия", "автобус", "без питания", 1);
+    private static Tourist tp_07 = new Tourist("Беларусь", "Витебск", "экскурсия", "поезд", "завтрак", 3);
+    private static Tourist tp_08 = new Tourist("Польша", "Белосток", "шопинг", "автобус", "без питания", 2);
+    private static Tourist tp_09 = new Tourist("Литва", "Вильнюс", "экскурсия", "поезд", "двухразовое", 4);
+    private static Tourist tp_10 = new Tourist("Латвия", "Рига", "отдых", "автобус", "трехразовое", 9);
+    private static Tourist tp_11 = new Tourist("Латвия", "Юрмала", "отдых", "поезд", "завтрак", 7);
+    private static Tourist tp_12 = new Tourist("Турция", "Аланья", "отдых", "самолет", "все включено", 11);
+    private static Tourist tp_13 = new Tourist("Турция", "Аланья", "отдых", "самолет", "все включено", 14);
+    private static Tourist tp_14 = new Tourist("Турция", "Аланья", "отдых", "самолет", "все включено", 5);
+    private static Tourist tp_15 = new Tourist("Турция", "Анталия", "отдых", "самолет", "все включено", 11);
+    private static Tourist tp_16 = new Tourist("Турция", "Анталия", "отдых", "самолет", "завтрак", 5);
+    private static Tourist tp_17 = new Tourist("Италия", "Неаполь", "круиз", "самолет", "трехразовое", 7);
+    private static Tourist tp_18 = new Tourist("Италия", "Генуя", "круиз", "самолет", "трехразовое", 5);
+    private static Tourist tp_19 = new Tourist("Испания", "Барселона", "экскурсия", "самолет", "завтрак", 4);
+    private static Tourist tp_20 = new Tourist("Южная Корея", "Чеджу", "экскурсия", "самолет", "трехразовое", 5);
+    static Tourist[] tpSet = new Tourist[]{tp_00, tp_01, tp_02, tp_03, tp_04, tp_05, tp_06, tp_07,
+            tp_08, tp_09, tp_10, tp_11, tp_12, tp_13, tp_14, tp_15, tp_16, tp_17, tp_18, tp_19, tp_20};
+}
