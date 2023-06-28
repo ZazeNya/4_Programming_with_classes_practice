5df36d8774f9ccd956df5b889c00b2d8c463fb27
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-28 14:19:57 +0300

    Create

---
+++ Aggregation and composition/Problem_3/Region.java
@@ -0,0 +1,52 @@
+package study;
+
+public class Region {
+    private String name;
+    private District[] districts;
+
+    public Region(String name, District... districts) {
+        this.name = name;
+        this.districts = districts;
+    }
+
+    //вычисление площади области
+    public double areaRegion() {
+        double area = 0;
+        for (District distr : districts) {
+            area += distr.getArea();
+        }
+        return area;
+    }
+
+    //вычисление количества населения
+    public int numberPopulationRegion() {
+        int numPopulation = 0;
+        for (District distr : districts) {
+            numPopulation += distr.getPopulation();
+        }
+        return numPopulation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public District[] getDistricts() {
+        return districts;
+    }
+
+    public void setDistricts(District... districts) {
+        this.districts = districts;
+    }
+
+    @Override
+    public String toString() {
+        return '\'' + name + '\'' +
+                ": площадь - " + areaRegion() +
+                " кв. км., наcеление - " + numberPopulationRegion() + " чел.";
+    }
+}
